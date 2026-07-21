2cca256916fed750447b4ddbaf0d89d589bfb1c8
makisragkousis <makisragkousis@gmail.com>
2026-07-21 19:32:51 +0300

    Initial commit

---
+++ src/main/java/gr/aueb/cf/schoolapp/SchoolappApplication.java
@@ -0,0 +1,13 @@
+package gr.aueb.cf.schoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schoolapp10SsrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schoolapp10SsrApplication.class, args);
+    }
+
+}
