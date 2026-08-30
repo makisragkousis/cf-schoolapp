abc7fbf1f03a394488b1c356e1339c09d870f419
makisragkousis <makisragkousis@gmail.com>
2026-08-30 19:49:42 +0300

    feat: add crud method for pages

--- src/main/java/gr/aueb/cf/schoolapp/repository/UserRepository.java
+++ src/main/java/gr/aueb/cf/schoolapp/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package gr.aueb.cf.schoolapp.repository;
 
 import gr.aueb.cf.schoolapp.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -14,4 +16,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @EntityGraph(attributePaths = {"role", "role.capabilities"})
     Optional<User> findByUuidAndDeletedFalse(UUID uuid);
+
+    @EntityGraph(attributePaths = {"role", "capabilities"})
+    Page<User> findAllByDeletedFalse(Pageable pageable);
 }
